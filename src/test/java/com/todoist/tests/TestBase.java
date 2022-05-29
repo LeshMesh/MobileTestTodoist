a5a36cacc974ed8b944bf747ed1773df8debd52d
LeshMesh <ivlex37rus@gmail.com>
2022-05-29 13:15:08 +0300

    Added TestBase

--- src/test/java/com/todoist/tests/TestBase.java
+++ src/test/java/com/todoist/tests/TestBase.java
@@ -1,4 +1,42 @@
 package com.todoist.tests;
 
+import com.codeborne.selenide.Configuration;
+import com.todoist.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static com.todoist.drivers.DeviceDriver.getDeviceDriver;
+import static com.todoist.helpers.Attach.getSessionId;
+
 public class TestBase {
+    private static final String deviceHost = System.getProperty("deviceHost");
+
+    @BeforeAll
+    public static void setup() {
+        addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.browser = getDeviceDriver(deviceHost);
+        Configuration.browserSize = null;
+    }
+
+    @BeforeEach
+    public void startDriver() {
+        open();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        String sessionId = getSessionId();
+
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+
+        closeWebDriver();
+        Attach.video(sessionId);
+    }
 }
