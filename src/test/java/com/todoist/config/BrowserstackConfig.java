d0c4e444be8b71a0be045e6ed4866806a1de6d12
LeshMesh <ivlex37rus@gmail.com>
2022-06-03 10:43:40 +0300

    Delete key from config

--- src/test/java/com/todoist/config/BrowserstackConfig.java
+++ src/test/java/com/todoist/config/BrowserstackConfig.java
@@ -5,15 +5,8 @@ import org.aeonbits.owner.Config;
 @Config.Sources({"classpath:browserstack.properties"})
 public interface BrowserstackConfig extends Config {
 
-    @Key("username")
     String username();
-
-    @Key("password")
     String password();
-
-    @Key("app")
     String app();
-
-    @Key("url")
     String url();
 }
